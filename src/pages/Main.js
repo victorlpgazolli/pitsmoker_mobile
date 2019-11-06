87b6c1d9476331aa992321fe3ba298d51a7e51d3
Victor Gazolli <victorgazolli@compmac.local>
2019-11-06 12:01:56 -0300

    code refactoring

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,74 +1,65 @@
 
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, BackHandler, ToastAndroid, Easing, TouchableOpacity, StyleSheet, Text, TextInput } from 'react-native'
 import io from "socket.io-client";
 import LinearGradient from 'react-native-linear-gradient';
 import { AnimatedCircularProgress } from 'react-native-circular-progress';
 import RNExitApp from 'react-native-exit-app';
-
-var lurl = 'http://pit-smoker-backend.herokuapp.com'
-
-export default class Main extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      fill: 10
-    };
-  }
-  componentDidMount() {
-    this.socket = io.connect(lurl);
-    BackHandler.addEventListener('hardwareBackPress', this.handleBackButton);
-    this.setState({ fill: 80 })
-    this.socket.on("user alive", msg => {
+// var lurl = 'http://pit-smoker-backend.herokuapp.com'
+var lurl = 'http://192.168.0.104:3333'
+var socket;
+socket = io.connect(lurl);
+export default function Main({ navigation }) {
+  const [temperature, setTemp] = useState(80)
+  const [recipe, setRecipe] = useState([])
+  const [account, setAccount] = useState([])
+  useEffect(() => {
+    setAccount(JSON.stringify(navigation.state.params.usuario))
+    setRecipe(JSON.stringify(navigation.state.params.receita))
+    BackHandler.addEventListener('hardwareBackPress', handleBackButton);
+    socket.on("user alive", msg => {
       setInterval(() => io.emit('user alive', "user alive"), 5000);
     })
-
-    this.socket.on("data", msg => {
-      ToastAndroid.show(`Dados recebidos: ${parseInt(msg)}`, ToastAndroid.SHORT)
-      this.setState({ fill: parseInt(msg) });
+    socket.on("data", msg => {
+      setTemp(parseInt(msg));
     });
-  }
-  // REMOVE BACK BUTTON
+  }, [])
 
-  componentWillUnmount() {
-    BackHandler.removeEventListener('hardwareBackPress', this.handleBackButton);
-  }
-
-  handleBackButton() {
+  console.log(`USER: ${account} - RECEITA: ${recipe}`)
+  var handleBackButton = function () {
     RNExitApp.exitApp()
     return true;
   }
 
-  render() {
-    return (
-      // <View style={styles.container}>
-      <LinearGradient colors={['#121212', '#171717', '#2B2B2B']} style={styles.linearGradient}>
-        <View style={[styles.tempInfo]}>
-          <AnimatedCircularProgress
-            easing={Easing.quad}
-            duration={800}
-            size={150}
-            width={5}
-            fill={this.state.fill}
-            ref={(ref) => this.circularProgress = ref}
-            tintColor="#eb4034"
-            arcSweepAngle={180}
-            rotation={270}
-            backgroundColor="#373737">
-            {
-              (fill) => (
-                <Text style={[styles.circleText]}>
-                  {this.state.fill}
-                </Text>
-              )
-            }
-          </AnimatedCircularProgress>
-        </View>
+  return (
+    // <View style={styles.container}>
+    < LinearGradient colors={['#121212', '#171717', '#2B2B2B']} style={styles.linearGradient} >
+      <View style={[styles.tempInfo]}>
+        <Text style={[styles.buttontext, { marginVertical: 20 }]}>{recipe.name}</Text>
+        <AnimatedCircularProgress
+          easing={Easing.quad}
+          duration={800}
+          size={150}
+          width={5}
+          fill={temperature}
+          ref={(ref) => circularProgress = ref}
+          tintColor="#eb4034"
+          arcSweepAngle={180}
+          rotation={270}
+          backgroundColor="#373737">
+          {
+            () => (
+              <Text style={[styles.circleText]}>
+                {temperature}
+              </Text>
+            )
+          }
+        </AnimatedCircularProgress>
+      </View>
 
-      </LinearGradient>
-    );
-  }
+    </LinearGradient >
+  )
+
 };
 
 const styles = StyleSheet.create({
@@ -87,5 +78,10 @@ const styles = StyleSheet.create({
   tempInfo: {
     alignItems: 'center',
     marginTop: 50
+  },
+  buttontext: {
+    color: "#FFF",
+    fontWeight: 'bold',
+    fontSize: 16
   },
 });
